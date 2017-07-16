8f405121e76947eb97778d773410b5e0b3c44ab2
ADOBENET\ugupta <uditgupta018@gmail.com>
2017-07-16 17:36:37 +0530

    making red black tree in progress

--- src/redblacktree.c
+++ src/redblacktree.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<malloc.h>
 
+//Keypoints while thinking algo :-
+//1. At every step, think if the current node pointer exists, i.ee it is not NULL
+//2. take node by node, like think if n==1 , then  n==2 formulate all cases
+//   then n==3, formulate all cases, this way help you design the algorithm yourself. 
+//3. take pen and paper and write on paper. 
+
 //this is the structure for our tree node of red black tree
 //data is value of tree node.
 //leftchild and rightchild are pointers to their children.
@@ -19,9 +25,8 @@ struct treenode {
 //it return *treenode pointer;
 //data is asisgned value "num"
 //both leftchild and rightchild are assigned NULL;
-//initialising color as black, we can assign with red also
-//as color of each node is handled while inserting or deleting, 
-//so it doesn't matter initial value as red/black.
+//initialising color as red, as when we inserting a new node
+//we are assigning it as "RED", 
 
 struct treenode* createNode(int num) {
 	struct treenode *ptr;
@@ -31,7 +36,7 @@ struct treenode* createNode(int num) {
 	ptr->right = NULL;
 	ptr->parent = NULL;
 	
-	ptr->color = 'black';
+	ptr->color = 'red';
 	
 	return ptr;
 }
@@ -51,8 +56,60 @@ struct treenode* binarySearchTreeInsert(struct treenode *root, struct treenode *
 	return root;
 }
 
-struct treenode* makeRedBlackTree(struct treenode *root, struct treenode *ptr){
+//Algorithm Insertion:- 
+//1. make the Root as black
+//2. if current node is red and its parent is also red then we have double red problem   
+
+struct treenode* makeRedBlackTree(struct treenode *root, struct treenode *ptr){        
+	struct treenode *parent_ptr, *grandparent_ptr, *uncle_ptr;
 	
+	//1. step
+	root->color = "black";
+
+	while(1){
+		//if current pointer is root, we have reached the root so break form loop
+		//or if ptr is NOT root then both ptr and ptr->parent exists
+		
+		if( (ptr == root) || ptr->color != 'red' || ptr->parent->color != 'red'){
+			break;
+		}        
+		
+		parent_ptr = ptr->parent;
+		grandparent_ptr = parent->ptr;
+		//if uncle is red , only re-coloring is required
+		//let's find the uncle pointer.
+		if(parent_ptr == grandparent_ptr->left){
+			uncle_ptr = grandparent_ptr->right;
+		}
+		else if(parent_ptr == grandparent_ptr->right){
+			uncle_ptr = grandparent_ptr->left;
+		}
+		
+		//if uncle pointer is red, only re-coloring is required 
+		//if it i NULL, it is considered as black;
+		if(uncle_ptr !=NULL && uncle_ptr->color == 'red'){
+			parent_ptr->color = uncle_ptr->color = 'black';
+			grandparent_ptr->color = 'red';
+			ptr = grandparent_ptr;
+		}
+		
+		else if(uncle_ptr == NULL || uncle_ptr->color == 'black'){
+			//Now we need to handle four cases of rotations
+			
+			//1. If ptr is left sub-child of grandparent 
+			if(ptr->parent == grandparent->left){
+				
+			}
+			
+			
+			//2. If ptr is right sub-child of grandparent
+			else if(ptr->parent == grandparent->right){
+				
+			}
+		}
+					
+	}
+	return ;
 }
 
 int main(){
